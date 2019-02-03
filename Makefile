ffac496af1a2f3ac65ba0f32d172792577b81a05
Dan Burke <dburke@addictmud.org>
2019-02-03 16:31:06 -0500

    add GET test

--- Makefile
+++ Makefile
@@ -8,7 +8,7 @@ build:
 	go build
 
 test:
-	go test ./...
+	go test ./... -v
 
 static:
 	CGO_ENABLED=0 go build -x -ldflags '-w -extldflags "-static"'
---
+++ api/get_test.go
@@ -0,0 +1,64 @@
+package api_test
+
+import (
+	//"bytes"
+	//"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"reflect"
+	"testing"
+
+	//"github.com/addictmud/types"
+	"github.com/gorilla/mux"
+	//"github.com/spf13/viper"
+
+	"github.com/dwburke/mockapi/api"
+	"github.com/dwburke/mockapi/config"
+)
+
+func setup() (r *mux.Router) {
+	r = mux.NewRouter()
+
+	config.Config = &config.ConfigType{
+		Routes: map[string]*config.Route{
+			"/get/thing/{id}": &config.Route{
+				Method: "GET",
+				Result: "{\"thing-id\":{{- .Params.id -}}}",
+			},
+		},
+	}
+
+	r.HandleFunc("/get/thing/{id}", api.MockGet).Methods("GET")
+
+	return
+}
+
+func TestMockGet(t *testing.T) {
+
+	r := setup()
+
+	req, _ := http.NewRequest("GET", "/get/thing/667", nil)
+	req.Header.Set("Content-Type", "application/json")
+	w := httptest.NewRecorder()
+
+	r.ServeHTTP(w, req)
+
+	body, err := ioutil.ReadAll(w.Body)
+	expect(t, err, nil, "")
+
+	expect(t, w.Code, http.StatusOK, string(body))
+	expect(t, string(body), string("{\"thing-id\":667}"), string(body))
+
+	//var room types.Room
+	//err = json.Unmarshal(body, &room)
+	//expect(t, err, nil, "")
+	//expect(t, room.Vnum, 1265, "")
+
+}
+
+func expect(t *testing.T, a interface{}, b interface{}, body string) {
+	if a != b {
+		t.Errorf("Expected [%v] (type %v) - Got [%v] (type %v) : %s", b, reflect.TypeOf(b), a, reflect.TypeOf(a), body)
+	}
+}
